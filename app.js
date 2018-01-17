0502e9a6a8e7e1a0466d63fd107d0a180bbb8aed
bFraley <bFraley@users.noreply.github.com>
2018-01-17 06:41:29 -0500

    app.set port fix

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ const http = require('http');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || '3001';
-
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', port);
+app.set('port', process.env.PORT || '3001';);
 
 app.get('*', (req, res) => { 
     res.sendFile(path.join(__dirname, 'dist/index.html'));
