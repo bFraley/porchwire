781b12611b231470d79ec1669c6790d88aaf593d
bFraley <bFraley@users.noreply.github.com>
2018-01-17 22:43:40 -0500

    adds peerjs min, prior to new component onboarding a peer - ng new component is broken with the current package.json with this bug here: https://github.com/angular/angular-cli/issues/9194

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || '3001';
 
+var peerserver = require('peer').ExpressPeerServer;
+
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', port);
 
-app.get('*', (req, res) => { 
+app.get('/', (req, res) => { 
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('Porchwire'));
+
+app.use('/porch', peerserver(server));
+
+server.on('connection', () => console.log('peer at /connect'));
